cf809a3fa6195e97f4b95fc3179059733888168e
Anurag Agrawal <anuragagrawal2026@gmail.com>
2026-08-16 23:20:17 +0530

    Update CI/CD and application

--- backend/server.js
+++ backend/server.js
@@ -32,8 +32,8 @@ app.get('/', (req, res) => {
 app.get('/test', async(req,res) => {
     res.json({
         test: "test was successful"
-    })
-})
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`);
